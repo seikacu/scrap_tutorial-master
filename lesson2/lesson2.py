80750303928f00bf786db404fb2f479f151719a5
neoxak <neoxak@gmail.com>
2023-05-25 22:07:20 +0300

    done

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -50,56 +50,81 @@ headers = {
 
 with open("lesson2/all_categories_dict.json", encoding="utf-8") as file:
     all_categories = json.load(file)
-    
+
 # print(all_categories)
 
-# iteration_count = int(len(all_categories)) - 1
+iteration_count = int(len(all_categories)) - 1
 count = 0
-# print(f"Всего итераций: {iteration_count}")
+print(f"Всего итераций: {iteration_count}")
 
 for category_name, category_href in all_categories.items():
 
-    if count == 0:
-        rep = [",", " ", "-", "'"]
-        for item in rep:
-            if item in category_name:
-                category_name = category_name.replace(item, "_")
-        # print(category_name)
-        req = requests.get(url=category_href, headers=headers)
-        src = req.text
+    # if count == 0:
+    rep = [",", " ", "-", "'"]
+    for item in rep:
+        if item in category_name:
+            category_name = category_name.replace(item, "_")
+    # print(category_name)
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
 
-        with open(f"lesson2/data/{count}_{category_name}.html", "w", encoding="utf-8") as file:
-            file.write(src)
+    with open(f"lesson2/data/{count}_{category_name}.html", "w", encoding="utf-8") as file:
+        file.write(src)
 
-        
-        
+    with open(f"lesson2/data/{count}_{category_name}.html", encoding="utf-8") as file:
+        src = file.read()
 
-        with open(f"lesson2/data/{count}_{category_name}.html", encoding="utf-8") as file:
-            src = file.read()
+    soup = BeautifulSoup(src, "lxml")
 
-        soup = BeautifulSoup(src, "lxml")
+    # проверка страницы на наличие таблицы с продуктами
+    alert_block = soup.find(class_="uk-alert-danger")
+    if alert_block is not None:
+        continue
 
-#     # проверка страницы на наличие таблицы с продуктами
-#     alert_block = soup.find(class_="uk-alert-danger")
-#     if alert_block is not None:
-#         continue
+    # собираем заголовки таблицы
+    table_head = soup.find(
+        class_="mzr-tc-group-table").find("tr").find_all("th")
 
-        # собираем заголовки таблицы
-        table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
-    
-        product = table_head[0].text
-        calories = table_head[1].text
-        proteins = table_head[2].text
-        fats = table_head[3].text
-        carbohydrates = table_head[4].text
-        # print(carbohydrates)
+    product = table_head[0].text
+    calories = table_head[1].text
+    proteins = table_head[2].text
+    fats = table_head[3].text
+    carbohydrates = table_head[4].text
+    # print(carbohydrates)
 
+    with open(f"lesson2/data/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                product,
+                calories,
+                proteins,
+                fats,
+                carbohydrates
+            )
+        )
 
-        with open(f"lesson2/data/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
+    # собираем данные продуктов
+    products_data = soup.find(
+        class_="mzr-tc-group-table").find("tbody").find_all("tr")
+
+    product_info = []
+    for item in products_data:
+        product_tds = item.find_all("td")
+
+        title = product_tds[0].find("a").text
+        calories = product_tds[1].text
+        proteins = product_tds[2].text
+        fats = product_tds[3].text
+        carbohydrates = product_tds[4].text
+
+        # print(fats)
+
+        with open(f"lesson2/data/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(
                 (
-                    product,
+                    title,
                     calories,
                     proteins,
                     fats,
@@ -107,59 +132,27 @@ for category_name, category_href in all_categories.items():
                 )
             )
 
+        product_info.append(
+            {
+                "Title": title,
+                "Calories": calories,
+                "Proteins": proteins,
+                "Fats": fats,
+                "Carbohydrates": carbohydrates
+            }
+        )
 
+    with open(f"lesson2/data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
+        json.dump(product_info, file, indent=4, ensure_ascii=False)
 
-#     # собираем данные продуктов
-        products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+    count += 1
 
-#     product_info = []
-        for item in products_data:
-            product_tds = item.find_all("td")
+    print(f"# Итерация {count}. {category_name} записан...")
+    iteration_count = iteration_count - 1
 
-            title = product_tds[0].find("a").text
-            calories = product_tds[1].text
-            proteins = product_tds[2].text
-            fats = product_tds[3].text
-            carbohydrates = product_tds[4].text
-            
-            print(fats)
+    if iteration_count == 0:
+        print("Работа завершена")
+        break
 
-
-            with open(f"lesson2/data/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
-                writer = csv.writer(file)
-                writer.writerow(
-                    (
-                        title,
-                        calories,
-                        proteins,
-                        fats,
-                        carbohydrates
-                    )
-                )
-                
-        count += 1
-
-
-#         product_info.append(
-#             {
-#                 "Title": title,
-#                 "Calories": calories,
-#                 "Proteins": proteins,
-#                 "Fats": fats,
-#                 "Carbohydrates": carbohydrates
-#             }
-#         )
-
-#     with open(f"data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
-#         json.dump(product_info, file, indent=4, ensure_ascii=False)
-
-#     count += 1
-#     print(f"# Итерация {count}. {category_name} записан...")
-#     iteration_count = iteration_count - 1
-
-#     if iteration_count == 0:
-#         print("Работа завершена")
-#         break
-
-#     print(f"Осталось итераций: {iteration_count}")
-#     sleep(random.randrange(2, 4))
+    print(f"Осталось итераций: {iteration_count}")
+    sleep(random.randrange(2, 4))
